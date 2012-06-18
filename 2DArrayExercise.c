a440234b44bedcb990e239e232837c38df028e9b
patback66 <maskoken@gmail.com>
2012-06-18 09:22:15 -0700

    update 2D array exercise

--- 2DArrayExercise.c
+++ 2DArrayExercise.c
@@ -19,26 +19,113 @@ int topScore()
 	}
 	int i,j,top;
 	*/
-for(i=0; i<100; i++)
-{
-	totals[i]=0;
-}
-for(i=0;i<100;i++)
-{
-	for(j=0; j<20; j++)
-	{	
-		totals[i]=totals[i]+hws[i][j];
-	}
-}
-highScore = 0;
-topStudent = 0;
-for(i=0; i<100; i++)
-{
-	if(totals[i]>highScore)
+	for(i=0; i<100; i++)
 	{
-		topstudent = i;
+		totals[i]=0;
 	}
-}
-return topstudent;
+	for(i=0;i<100;i++)
+	{
+		for(j=0; j<20; j++)
+		{	
+			totals[i]=totals[i]+hws[i][j];
+		}
+	}
+	highScore = 0;
+	topStudent = 0;
+	for(i=0; i<100; i++)
+	{
+		if(totals[i]>highScore)
+		{
+			topstudent = i;
+		}
+	}
+	return topstudent;
 }
 
+int toughestHomework();
+{
+	int lowIndex = 0;
+	int lowAverage = 100;
+	int sum;
+	int average;
+	for (int indexHomework; indexHomework < 20; indexHomework++)
+	{
+		sum = 0;
+		for(int studentIndex = 0; studentIndex < 100; studentIndex++)
+		{
+			sum+=hws[studentIndex][indexHomework];
+		}
+		average = sum/100;
+		if(average < lowAverage)
+		{
+			lowAverage = average;
+			lowIndex = indexHomework;
+		}
+
+	}
+}
+
+//teacher answer
+int tough()
+{
+	int thwsofar=0;
+	int softhwsofar = 100000000000000;
+	for (j=0;j<20;j++)
+	{
+		sofhwj=0;
+		for(i=0;i<100;i++)
+		{
+			sohwj=sofhwj+hws[i][j];
+		}
+		if(sofhwj<softhwsofar)
+		{
+			thwsofar=j;
+			softhwsofar=sofhwj;
+		}
+	}
+	return thwsofar;
+}
+
+int numhighest();
+{
+	int highGrade = 0;
+	int count = 0;
+	int numOccurrences
+	for(int i=0; i<100; i++)
+	{
+		for (int j=0; j<20; j++)
+		{
+			if (hws[i][j] > highGrade)
+			{
+				highGrade = hws[i][j]j
+			}
+		}
+	}
+	for (i=0; i<100; i++)
+	{
+		for(j=0; j<80; j++)
+		{
+			if(hws[i][j]==highGrade)
+			{
+				numOccurrences++;
+			}
+		}
+	}
+}
+/*
+for(i=0; i<100; i++)
+{
+	for (j=0; j<20; j++)
+	{
+		if (hws[i][j]>highestScore)
+		{
+			highestScore = hws[i][j];
+			numOccurrences=1;
+		}
+		else if(hws[i][j] == highestScore)
+		{
+			numOccurrences++;
+		}
+	}
+}
+*/
